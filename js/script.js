ffca1a9dc2b38ec32f601353ee738140d24e59d9
artikosamodra <artikosamudra@gmail.com>
2023-12-18 15:29:33 +0700

    Add fungsi vDesc... tu use text desc by id

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
+let vDesc = {
+    'btnV1': "BEACH. This Text Descript Area",
+    'btnV2': "FOREST. This Text Descript Area",
+    'btnV3': "HARBOUR. This Text Descript Area",
+    'btnV4': "SEA. This Text Descript Area",
+    'btnV5': "TOWN. This Text Descript Area"
+  }; 
+
+
+
 function btnVid(button){
     let playerVid = document.getElementById('playerVid');
+    let textDesc = document.getElementById('textDesc');
+
+    //Selector to PlayerVid.source
     let source = playerVid.querySelector('source');
 
-    let videoSrc = button.getAttribute('video-src');
 
+    //get attribute video-src from button.
+    let videoSrc = button.getAttribute('video-src');
+    let textSrc = button.getAttribute('id');
+
+    //add or change source.src value >> video-src value at videoSrc >
     source.src = videoSrc;
+    textDesc.textContent = vDesc[textSrc];
 
     playerVid.load();
     playerVid.play();
@@ -12,5 +30,3 @@ function btnVid(button){
     console.log(videoSrc);
 
 }
-
-
